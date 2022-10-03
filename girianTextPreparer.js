f7cd2fc2d8b9b2b34c4ad20988828bccddaa6d39
Swarup Giri <74725133+Swarup-Giri@users.noreply.github.com>
2022-10-03 11:24:02 -0500

    Delete girianTextPreparer.js

--- girianTextPreparer.js
+++
@@ -1,8 +0,0 @@
-function format(text = "") {
-    var returnText = text
-    returnText = returnText.replaceAll("? ", ". ").replaceAll("! ", ". ")
-    returnText = returnText.replaceAll(". ", " | ").replaceAll(", ", " || ").replaceAll("\n", " ").replaceAll("	", "")
-    return returnText
-}
-var text = `hello this is an example of some text, so um yeah any ways what is the point of .life`
-console.log(format(text))
