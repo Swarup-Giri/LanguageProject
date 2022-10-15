0206edd276f5158766dadac4283e7a2955b8a9d8
Swarup Giri <swarupgiree@gmail.com>
2022-10-15 11:38:37 -0500

    Merge branch 'main' of https://github.com/Swarup-Giri/Javascript

--- girianLanguageScript.py
+++ girianLanguageScript.py
@@ -1,3 +1,4 @@
+from pickletools import read_uint1
 from numpy import Infinity
 
 
@@ -8,7 +9,7 @@ def replace(value = ""):
     value = value.replace("kc", "k").replace("ck", "k")
     value = value.replace("wh", "w").replace("hw", "h")
     value = value.replace("q", "k").replace("x", "k")
-    value = value.replace("ae", "a").replace("oe", "o").replace("ea", "e").replace("eo", "e")
+    value = value.replace("ae", "a").replace("oe", "o").replace("ea", "e").replace("eo", "e").replace("ao", "a").replace("oa", "o")
     value = value.replace("ee", "e").replace("ee", "e").replace("ee", "e").replace("ee", "e")
     value = value.replace("aa", "a").replace("aa", "a").replace("aa", "a").replace("aa", "a")
     value = value.replace("oo", "o").replace("oo", "o").replace("oo", "o").replace("oo", "o")
@@ -27,7 +28,6 @@ varient = {
     "formal": "formal",
     "northern": "northern",
     "southern": "southern",
-    "eastern": "eastern",
     "western": "western",
     "old": "old",
     "rude": "rude"
@@ -52,7 +52,7 @@ def translate (text = "", varient = "informal", simplified = True, type = "m"):
     text = text.replace("\n", " ").replace("	", " ")
     while (text != text.replace("  ", " ")):
         text = text.replace("  ", " ")
-    print(GETOLF(text, varient, type, simplified))
+    print(GETOLF(text, varient))
 def GETOL(textString = "", type = "m"):
     returnText = textString.lower()
     returnText = returnText[::-1] # [::-1] means reverse
@@ -76,8 +76,6 @@ def GETOLF(text = "", varient = "informal", type = "m", simlified = True):
         varient = 4
     elif varient == "southern":
         varient = 2
-    elif varient == "eastern":
-        varient == 2
     elif varient == "western":
         varient = 6
     elif varient == "informal":
@@ -86,24 +84,60 @@ def GETOLF(text = "", varient = "informal", type = "m", simlified = True):
         varient = Infinity
     returnData = returnData.split(" ")
     for i in range(len(returnData)):
-        if ((returnData[i] != "|") and (returnData[i] != "||") and ("0" not in returnData[i]) and ("1" not in returnData[i]) and ("2" not in returnData[i]) and ("3" not in returnData[i]) and ("4" not in returnData[i]) and ("5" not in returnData[i]) and ("6" not in returnData[i]) and ("7" not in returnData[i]) and ("8" not in returnData[i]) and ("9" not in returnData[i]) and (not (returnData[i][0] == "."))):
+        if ((returnData[i] != "|") and (returnData[i] != "||") and ("0" not in returnData[i]) and ("1" not in returnData[i]) and ("2" not in returnData[i]) and ("3" not in returnData[i]) and ("4" not in returnData[i]) and ("5" not in returnData[i]) and ("6" not in returnData[i]) and ("7" not in returnData[i]) and ("8" not in returnData[i]) and ("9" not in returnData[i]) and (not (returnData[i] == "."))):
             if (returnData[i] == ""):
                 break
             elif varient == -1:
                 returnData[i] = GETOL(GETOL(GETOL(returnData[i], type), type), type)
-            elif len(returnData[i] >= varient):
+            elif len(returnData[i]) >= varient:
                 returnData[i] = GETOL(returnData[i], type)
             elif (len(returnData[i]) > 2) and (varient < 7):
                 returnData[i] = returnData[i][::-1].lower()
                 minusOne = returnData[i][-1]
                 minusTwo = returnData[i][-2] if len(returnData[i]) >= 2 else ""
-                minusFour = returnData[i][-4] if len(returnData[i]) >= 4 else ""
+                minusThree = returnData[i][-3] if len(returnData[i]) >= 3 else ""
                 minusFive = returnData[i][-5] if len(returnData[i]) >= 5 else ""
                 returnData[i] = returnData[i].replace(minusOne, "").replace(minusTwo, "")
                 returnData[i] = returnData[i].replace(returnData[i][0], minusOne, 1)
+                returnData[i] = f"{minusOne}e{minusFive}e{minusThree}o{minusOne}{minusTwo}a{returnData[i]}am"
+            else:
+                returnData[i] = returnData[i][::-1]
+                returnData[i] = remove_consecutive_duplicates(returnData[i])
+                minusOne = returnData[i][-1]
+                minusTwo = returnData[i][-2] if len(returnData[i]) >= 2 else ""
+                minusThree = returnData[i][-3] if len(returnData[i]) >= 3 else ""
+                minusFour = returnData[i][-4] if len(returnData[i]) >= 4 else ""
+                minusFive = returnData[i][-5] if len(returnData[i]) >= 5 else ""
+                returnData[i] = list(returnData[i])
+                returnData[i][-1] = returnData[i][0]
+                returnData[i][0] = minusOne
+                returnData[i] = "".join(returnData[i])
                 returnData[i] = f"{minusFour}a{returnData[i]}"
-                # COMPLETED UNTIL LINE 178
-print(replace("eeeeeeeeeeeeeeeeen this is an example of something haha"))
-print(remove_consecutive_duplicates("hiiii"))
-print(format("hello. this is something, I think? or is it? I really don't know. "))
-print(GETOL("hello"))
+        
+        if not(returnData[i][0] == ".") and True and not(varient > 100):
+            if (varient == "informal"): #COME BACK TO LATER
+                text = remove_consecutive_duplicates(text)
+            returnData[i] = replace(returnData[i])
+        elif returnData[i][0] == ".":
+            returnData[i] = returnData[i].replace(".", "\"")
+            returnData[i]  = returnData[i] + "\""
+        elif varient > 100:
+            returnData[i] = replace(returnData[i])
+            if returnData[i][len(returnData[i]) - 1] == "n":
+                returnData[i] = list(returnData[i])
+                returnData[i][-1] = "o"
+                returnData[i] = "".join(returnData[i])
+    returnData = " ".join(returnData)
+    #returnData = returnData.replace(",", " ")
+    returnData = returnData.replace(" || ", ", ").replace(" |", "| ").replace("  ", " ").replace("  ", " ")
+    if varient > 100:
+        returnData = remove_consecutive_duplicates(returnData)
+    return returnData
+"""translateText = "hello, this is an example of girian text in this specific varient"
+translate(translateText, varient["normal"])
+translate(translateText, varient["informal"])
+translate(translateText, varient["formal"])
+translate(translateText, varient["northern"])
+translate(translateText, varient["southern"])
+translate(translateText, varient["western"])
+translate(translateText, varient["old"])"""
