2226e4cbda3556fbabe72f6ff19ffc4d0485033d
Swarup Giri <swarupgiree@gmail.com>
2022-10-24 11:41:03 -0500

    New Script...

---
+++ girianLanguageScriptImproved.js
@@ -0,0 +1,8 @@
+function translate(text = "", [formality = 0, region = 0], returnType = false) {
+    var returnText = text
+    returnText = returnText
+
+
+    if (returnType) { return returnText } console.log(returnText)
+}
+translate("hi", [0, 2])
