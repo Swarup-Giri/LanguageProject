d18fe605cd557082666769e547a91dc6cdc91a02
Swarup Giri <swarupgiree@gmail.com>
2022-10-25 17:15:58 -0500

    Changes...

--- girianLanguageScriptImproved.js
+++ girianLanguageScriptImproved.js
@@ -1,8 +1,25 @@
-function translate(text = "", [formality = 0, region = 0], returnType = false) {
+function translate(text = "", returnType = false) {
     var returnText = text
-    returnText = returnText
-
-
+    returnText = returnText.replaceAll(".", " . ").replaceAll(",", " , ").replaceAll("!", " ! ").replaceAll("?", " ? ").replaceAll(":", " : ").replaceAll(";", " ; ").replaceAll("\"", " \" ").replaceAll("\'", " \' ")
+    returnText = returnText.split(" ")
+    for (var i = 0; i < returnText.length; i++) {
+        if (i < returnText.length-1) {
+            if (returnText[i + 1].length < 5) {
+                returnText[i] = returnText[i] + returnText[i + 1]
+                returnText[i + 1] = ""
+            }
+        }
+        returnText[i] = translateIndividual(returnText[i])
+    }
+    returnText = returnText.join(" ").replaceAll(" . ", ".").replaceAll(" , ", ",").replaceAll(" ! ", "!").replaceAll(" ? ", "?").replaceAll(" : ", ":").replaceAll(" ; ", ";").replaceAll(" \" ", "\"").replaceAll(" \' ", "\'").replaceAll("  ", " ").replaceAll("  ", " ")
     if (returnType) { return returnText } console.log(returnText)
 }
-translate("hi", [0, 2])
+function translateIndividual(text = "") {
+    var returnText = text
+    returnText = returnText.split("").reverse().join("")
+    firstLast = returnText[returnText.length-1]
+    
+
+    return returnText
+}
+translate("hi this is someone. haha?")
